10da6e69e384ffbde32b968417eda8347671370a
Christine Yu <christine.tc.yu@gmail.com>
2015-06-17 00:00:56 -0400

    feat(init): start over

---
+++ src/android/TessCordova.java
@@ -0,0 +1,36 @@
+package com.tesscordova;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class TessCordova extends CordovaPlugin {
+
+    public TessCordova() {
+        super();
+    }
+
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        if ("coolMethod".equals(action)) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
