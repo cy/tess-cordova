935637d500b8b9ca3fa24bf7e5ae775a88d88cd9
Christine Yu <christine.tc.yu@gmail.com>
2015-06-21 00:30:29 -0400

    feat(android): wip - copy tessdata to external storage

--- src/android/TessCordova.java
+++ src/android/TessCordova.java
@@ -14,17 +14,28 @@ import android.graphics.BitmapFactory;
 import android.graphics.Bitmap;
 import android.media.ExifInterface;
 import android.graphics.Matrix;
+import android.os.Environment;
+import android.content.res.AssetManager;
 
 import java.io.IOException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
 public class TessCordova extends CordovaPlugin {
     static final String IMAGE_PATH = "file:///android_asset/receipt.jpg";
+    public static final String DATA_PATH = Environment
+            .getExternalStorageDirectory().toString() + "/TessCordova/";
+    public static final String LANG = "eng";
+
     static final String LOG_TAG = "com.tesscordova";
 
     public TessCordova() {
         super();
+        copyTrainedData();
     }
 
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -34,6 +45,7 @@ public class TessCordova extends CordovaPlugin {
             this.coolMethod(message, callbackContext);
             return true;
         } else if ("tessOCR".equals(action)) {
+
              BitmapFactory.Options options = new BitmapFactory.Options();
              options.inSampleSize = 2;
              Bitmap bitmap = BitmapFactory.decodeFile(IMAGE_PATH, options);
@@ -73,7 +85,7 @@ public class TessCordova extends CordovaPlugin {
         }
 
             TessBaseAPI baseApi = new TessBaseAPI();
-            baseApi.init("file:///android_asset/tessdata", "eng");
+            baseApi.init(DATA_PATH, LANG);
 
             return true;
         }
@@ -88,5 +100,51 @@ public class TessCordova extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    private void copyTrainedData() {
+        String[] paths = new String[] { DATA_PATH, DATA_PATH + "tessdata/" };
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(LOG_TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(LOG_TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+
+        }
+        // LANG.traineddata file with the app (in assets folder)
+        // You can get them at:
+        // http://code.google.com/p/tesseract-ocr/downloads/list
+        // This area needs work and optimization
+        if (!(new File(DATA_PATH + "tessdata/" + LANG + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = this.cordova.getActivity().getApplicationContext().getAssets();
+                InputStream in = assetManager.open("tessdata/" + LANG + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + LANG + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+                Log.v(LOG_TAG, "Copied " + LANG + " traineddata");
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Was unable to copy " + LANG + " traineddata " + e.toString());
+            }
+        }
+
     }
 }
